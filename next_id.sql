2593469da8866f65caa564e3db4443c17a8ec4c0
Raja Sekhar Reddy Gajjala <shekhar.rj@gmail.com>
2025-07-09 12:57:07 -0400

    Updated the next id logic

--- next_id.sql
+++ next_id.sql
@@ -11,11 +11,12 @@ SELECT
 	o.airborne_lcl_tms,
 	o.landing_lcl_tms,
   r.id AS next_flight_id,
-  RANK () OVER (PARTITION BY o.actl_arr_lcl_tms ORDER BY r.actl_dep_lcl_tms) AS rnk
+  RANK () OVER (PARTITION BY o.acft_regs_cde, o.actl_arr_lcl_tms ORDER BY r.actl_dep_lcl_tms) AS rnk
 FROM "FPT Interview".main."FPT Interview" o
 LEFT JOIN "FPT Interview".main."FPT Interview" r
 ON o.orig = r.dest
 AND o.dest = r.orig
+AND o.acft_regs_cde = r.acft_regs_cde
 -- Assuming the return flight is scheduled within two days of arrival, this is to reduce the number of joins
 AND r.actl_dep_lcl_tms BETWEEN o.actl_arr_lcl_tms AND o.actl_arr_lcl_tms + INTERVAL '2 days'
 )
